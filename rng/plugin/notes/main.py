00cf47f2e91fef0038c03c6dcd63921d8b0005c1
Roberto Bravo <xbx.roberto@gmail.com>
2012-08-29 13:39:20 -0300

    Add tasks for notes plugin: add and find

--- rng/plugin/notes/main.py
+++ rng/plugin/notes/main.py
@@ -4,6 +4,7 @@ Created on Aug 22, 2012
 @author: xbx
 '''
 from plugin.base.main import BasePlugin
+from datetime import datetime
 
 
 class Notes(BasePlugin):
@@ -11,7 +12,22 @@ class Notes(BasePlugin):
     def task_default(self):
         print "nice plugin!"
 
-    def task_add(self):
-        self._input("enter note: ")
+    def task_add(self, parameters=None):
+        if parameters is not None:
+            text = parameters
+        else:
+            text = self._input("enter note: ")
+        note = {'text': text,
+                'timestamp': datetime.now()}
+        self.db.insert(note)
+
+    def task_find(self, parameters=None):
+        if parameters is None:
+            notes = self.db.find()
+        else:
+            # TODO custom parameters
+            notes = self.db.find(parameters)
+        for note in notes:
+            print "[%s]\n%s\n" % (note['timestamp'], note['text'])
 
 plugin_instance = Notes()
